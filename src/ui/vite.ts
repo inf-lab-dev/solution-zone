d1f2ffa0878eb58219721bd2d43cbcba60029606
Kuschel_Swein <kuschelswein@gmail.com>
2024-11-16 21:28:03 +0100

    Fix worker loading for vite

--- src/ui/vite.ts
+++ src/ui/vite.ts
@@ -1,6 +1,16 @@
 import { WorkerClass, WorkerOptions } from './monaco.js';
 
 /**
+ * A module that houses a worker.
+ */
+export interface WorkerModule {
+    /**
+     * The default export, the worker.
+     */
+    default: WorkerClass;
+}
+
+/**
  * Creates {@link WorkerOptions} using dynamically imported Vite modules.
  * As the `?worker` import-syntax is only supported for userland code, the imports have to be done manually.
  * To aid in importing, the {@link ViteWorkerUrl} enum can be used.
@@ -13,20 +23,19 @@ import { WorkerClass, WorkerOptions } from './monaco.js';
  * @returns the imported workers, needed for the editor
  */
 export async function createViteWorkerOptions(
-    editor: Promise<unknown>,
-    css: Promise<unknown>,
-    html: Promise<unknown>,
-    json: Promise<unknown>,
-    typescript: Promise<unknown>,
+    editor: Promise<WorkerModule>,
+    css: Promise<WorkerModule>,
+    html: Promise<WorkerModule>,
+    json: Promise<WorkerModule>,
+    typescript: Promise<WorkerModule>,
 ): Promise<WorkerOptions> {
-    const [editorWorker, cssWorker, jsonWorker, htmlWorker, tsWorker] =
-        (await Promise.all([
-            editor,
-            css,
-            json,
-            html,
-            typescript,
-        ])) as WorkerClass[];
+    const [
+        { default: editorWorker },
+        { default: cssWorker },
+        { default: jsonWorker },
+        { default: htmlWorker },
+        { default: tsWorker },
+    ] = await Promise.all([editor, css, json, html, typescript]);
 
     return {
         editor: editorWorker,
